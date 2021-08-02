5b93f6ae439a87b178844e06401defde36a7cde8
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-08-02 23:53:03 +0400

    Update switch.py

--- switch.py
+++ switch.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from . import __
+from switch import __
 
 class Switch(object):
     """Switch-Case class for Python"""
