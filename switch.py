2b07ec789f1dd309873677747a12ea75faa6a3e6
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-08-10 18:50:17 +0400

    Update switch.py

--- switch.py
+++ switch.py
@@ -34,18 +34,21 @@ class Switch(object):
 		"""
 		if isinstance(cases, dict) is False:
 			raise TypeError("Cases should be dict (dictionary)")
-		self.cases = cases
-		self.default = default
+		self.cases, self.default = cases, default
 	
 	def __repr__(self) -> str:
 		keys = list(self.cases.keys())
 		if len(keys) == 0:
-			return "Switch(key)"
+			return "Switch(key):\n\tEmpty"
 		out = "Switch(key):"
 		for key in keys:
-			if key != self.default:
-				out += f"\n\tCase({key}):\n\t\t{self.cases[key]}"
-		out += f"\n\tDefault({self.default}):\n\t\t{self.cases[self.default]}"
+			if key != self.default and key is not None:
+				val = self.cases[key]
+				if isinstance(val, str):
+					val = f'"{val}"'
+				out += f"\n\tCase({key}):\n\t\t{val}"
+		if self.default != None:
+			out += f"\n\tDefault({self.default}):\n\t\t{self.cases[self.default]}"
 		return out
 	
 	def case(self, key, value):
@@ -75,4 +78,4 @@ class Switch(object):
 		-------
 			value corresponding to key if it exists else default
 		"""
-		return self.cases[key] if key in self.cases else (self.cases[self.default] if self.default in self.cases else None)
+		return self.cases[key] if key in self.cases else (self.cases[self.default] if self.default in self.cases and self.default is not None else None)
