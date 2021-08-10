fe71937bf4616cc9b53d7083eaafcb111896497f
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-08-10 21:33:59 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup, find_packages
-setup(
-    name = 'switch',
-    version = '1.0.0',
-    author = 'Luka Mamukashvili',
-    author_email = 'ultraluka0@gmail.com',
-    description = ('Switch-Case class for Python'),
-    license = 'GPL-3.0',
-    url = 'https://github.com/UltraStudioLTD/Switch',
-    scripts = [],
-    py_modules = ['switch'],
-    packages = find_packages(),
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="switch",
+    version="1.0.0",
+    author="Luka Mamukashvili",
+    author_email="ultraluka0@gmail.com",
+    description="Switch-Case for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UltraStudioLTD/Switch",
+    project_urls={
+        "Bug Tracker": "https://github.com/UltraStudioLTD/Switch/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     install_requires = ['typing;python_version<"3.5"'],
-    download_url = 'https://github.com/UltraStudioLTD/Switch/tarball/master'
 )
